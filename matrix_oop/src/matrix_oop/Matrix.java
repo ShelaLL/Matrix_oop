1a8a865364b80d45fade7825086b66bacf88bd4b
ASUS <ASUS@DESKTOP-SDMPT9S>
2021-03-13 00:48:41 +0100

    Immutable API, column major, WPI

--- matrix_oop/src/matrix_oop/Matrix.java
+++ matrix_oop/src/matrix_oop/Matrix.java
@@ -21,9 +21,7 @@ public class Matrix {
 	private int nbRows;
 	/**
 	 * @representationObject
-	 * This is the representation object because our inspectors depend on it
 	 */
-	//???
 	private double[] elements;
 
 	/**
@@ -37,18 +35,17 @@ public class Matrix {
 	 * @inspects | this
 	 * @basic
 	 * @creates | result
-	 * Since this inspector returns a mutable object, we should specify whether it is an existing object or a new one
-	 * In this case, we create a new one, so we used the create clause.
-	 * Creates clause tells the client whether this object he will return that he will have complete control over
-	 * Guarantee to the client that this obejct will not be changed by class Matrix after at the end of this method calls
 	 */
 	public double[] getElementsRowMajor() {
-		return elements.clone();
+		double[] result = new double[elements.length];
+		for (int rowIndex = 0; rowIndex < nbRows; rowIndex++)
+			for (int columnIndex = 0; columnIndex < elements.length / nbRows; columnIndex++)
+				result[rowIndex * elements.length / nbRows + columnIndex] = elements[columnIndex * nbRows + rowIndex];
+		return result;
 	}
 
 	/**
 	 * @post | result == getElementsRowMajor().length / getNbRows()
-	 *  In any case we should not mention fields in a post condition or a public method???
 	 */
 	public int getNbColumns() {
 		return elements.length / nbRows;
@@ -58,13 +55,11 @@ public class Matrix {
 	 * @post | result != null
 	 * @post | result.length == getNbRows()
 	 * @post | Arrays.stream(result).allMatch(row -> row != null && row.length == getNbColumns())
-	 * Its elements should also not be null
 	 * @post | IntStream.range(0, getNbRows()).allMatch(rowIndex -> 
 	 * 		 | 		IntStream.range(0, getNbColumns()).allMatch(columnIndex ->
 	 * 		 | 		   result[rowIndex][columnIndex] == getElementsRowMajor()[rowIndex * getNbColumns() + columnIndex]))
 	 *       
 	 * @creates | result, ...result
-	 * same thing holds for the result of the getElementsRowArrays() method and also the elements of the resulting array are all new array objects
 	 */
 	public double[][] getElementsRowArrays() {
 		int nbColumns = getNbColumns();
@@ -72,14 +67,32 @@ public class Matrix {
 		for(int rowIndex = 0; rowIndex < nbRows; rowIndex++) {
 			rows[rowIndex] = new double[nbColumns];
 			for(int columnIndex = 0; columnIndex < nbColumns; columnIndex++) {
-				rows[rowIndex][columnIndex] = elements[rowIndex * nbColumns + columnIndex]; //???different from above
+				rows[rowIndex][columnIndex] = elements[columnIndex * nbRows + rowIndex]; 
 			}
 		}
 		return rows;
 	}
-	
+
 	//When we define the API, we should also document the constructor
-	public Matrix(int nbRows, int nbColumns, double[] elementsRowMajor) {throw new RuntimeException("Not yet implemented");}
+	public Matrix(int nbRows, int nbColumns, double[] elementsRowMajor) {
+		if(nbRows < 1)
+			throw new IllegalArgumentException("`nbRows` is less than 1");
+		if(nbColumns < 1)
+			throw new IllegalArgumentException("`nbColumns` is less than 1");
+		if(elementsRowMajor == null)
+			throw new IllegalArgumentException("`elementsRowajor` is null");
+		if(elementsRowMajor.length != nbRows * nbColumns)
+			throw new IllegalArgumentException("length of `elementsRowMajor` is worng");
+
+		this.nbRows = nbRows;
+		this.elements = new double[nbRows * nbColumns];
+
+		for(int rowIndex = 0; rowIndex < nbRows; rowIndex++) {
+			for(int columnIndex = 0; columnIndex < nbColumns; columnIndex++) 
+				elements[columnIndex * nbRows + rowIndex] = elementsRowMajor[rowIndex * nbColumns + columnIndex];
+
+		}
+	}
 }
 
 
