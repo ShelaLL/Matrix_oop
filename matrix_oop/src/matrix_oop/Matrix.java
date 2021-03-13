f99938f82ff180a2c5eefb93148505a55590173d
ASUS <ASUS@DESKTOP-SDMPT9S>
2021-03-13 01:17:51 +0100

    @immutable

--- matrix_oop/src/matrix_oop/Matrix.java
+++ matrix_oop/src/matrix_oop/Matrix.java
@@ -10,6 +10,8 @@ import java.util.stream.IntStream;
  * @invar | getElementsRowMajor() != null
  * @invar | getElementsRowMajor().length == getNbRows() * getNbColumns()
  * @invar | 1 <= getNbColumns()
+ * 
+ * @immutable
  */
 public class Matrix {
 
@@ -101,5 +103,4 @@ public class Matrix {
 		this.nbRows = nbRows;
 		this.elements = elementsRowMajor.clone();
 	}
-
 }
